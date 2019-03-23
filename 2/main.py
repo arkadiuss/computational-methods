4b4e5b32819778ef59effcdcbc5c572e0e7ae6b0
Arkadiusz Kraus <arkadius.kraus@gmail.com>
2019-03-23 10:03:14 +0100

    [2] 2, 3 graph

---
+++ 2/main.py
@@ -0,0 +1,25 @@
+class Edge:
+    def __init__(self, u, v):
+        self.u = u
+        self.v = v
+
+
+class Node:
+    def __init__(self):
+        self.edges = []
+
+
+class Graph:
+    def __init__(self, n):
+        self.n = n
+        self.G = [Node() for i in range(n)]
+
+    def add_edge(self, a, b, w):
+        e = Edge(b, w)
+        self.G[a].edges.append(e)
+        e = Edge(a, w)
+        self.G[b].edges.append(e)
+
+
+#    def import_from_file(file):
+
