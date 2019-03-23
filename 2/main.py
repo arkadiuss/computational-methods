3e92bd095b936b7779e935a4c33c8c590160006a
Arkadiusz Kraus <arkadius.kraus@gmail.com>
2019-03-23 10:31:29 +0100

    [2] add networkx

--- 2/main.py
+++ 2/main.py
@@ -1,39 +1,20 @@
 import numpy as np
 import os
 import csv
+import networkx as nx
 
 
-class Edge:
-    def __init__(self, u, v):
-        self.u = u
-        self.v = v
+def import_from_file(file):
+    records = []
+
+    with open(file, "r") as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            records.append(row)
+    #map(records
 
 
-class Node:
-    def __init__(self):
-        self.edges = []
-
-
-class Graph:
-    def __init__(self, n=0):
-        self.n = n
-        self.G = [Node() for i in range(n)]
-
-    def add_edge(self, a, b, w):
-        e = Edge(b, w)
-        self.G[a].edges.append(e)
-        e = Edge(a, w)
-        self.G[b].edges.append(e)
-
-    def import_from_file(self, file):
-        with open(file, "r") as csvfile:
-            reader = csv.reader(csvfile)
-            for row in reader:
-                self.n += 1
-
-
-
-g = Graph()
-g.import_from_file("soc-sign-bitcoinalpha.csv")
+G = nx.Graph()
+import_from_file("soc-sign-bitcoinalpha.csv")
 
 
