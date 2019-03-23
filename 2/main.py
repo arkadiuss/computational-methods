59a2702c621961564fe968b2861aaf60fc411f48
Arkadiusz Kraus <arkadius.kraus@gmail.com>
2019-03-23 11:41:53 +0100

    [2] equations solving

--- 2/main.py
+++ 2/main.py
@@ -14,7 +14,7 @@ def import_from_file(file):
     G.add_nodes_from(list(map(lambda x: int(x[0]), records)))
     for i, record in enumerate(records):
         print(record, i)
-        G.add_edge(int(record[0]), int(record[1]),  R=abs(int(record[2])), I=0, no=i)
+        G.add_edge(int(record[0]), int(record[1]),  R=abs(int(record[2])), no=i+1, sem=0)
         # G.add_edge(int(record[1]), int(record[0]),  R=abs(int(record[2])), I=0, no=i)
 
     return G
@@ -22,38 +22,48 @@ def import_from_file(file):
 
 def KirI(G):
     eqs = []
+    b = []
     edges_count = len(G.edges)
     for n in G.nodes:
         Is = [0 for i in range(edges_count)]
+        b.append(0)
         for e in G.edges(n):
             data = G.get_edge_data(*e)
             print(e, data['no'])
             Is[data['no']] = 1 if e[0] > e[1] else -1
         print('\n\n')
         eqs.append(Is)
-    return eqs
+    return eqs, b
 
 
 def KirII(G):
     eq = []
+    b = []
     edges_count = len(G.edges)
     for cycle in nx.cycle_basis(G):
+        print(cycle)
         Is = [0 for i in range(edges_count)]
+        b.append(0)
         for i in range(len(cycle)):
             n1 = cycle[i]
             n2 = cycle[(i+1) % len(cycle)]
             data = G.get_edge_data(n1,n2)
             Is[data['no']] = data['R'] if n1 > n2 else -data['R']
+            b[-1] += data['sem'] if n1 > n2 else -data['sem']
         eq.append(Is)
-    return eq
+    return eq, b
 
 
 def create_equations(G):
-    eqs = KirI(G)
-    eqs += KirII(G)
-    print(eqs)
+    eqs, b = KirI(G)
+    eqs2, b2 = KirII(G)
+    eqs = eqs + eqs2
+    b = b + b2
+    return eqs, b
 
 
 G = import_from_file("small.csv")
-create_equations(G)
-#print(G.edges)
+G.add_edge(1,4, R=0, sem=3, no=0)
+I, E = create_equations(G)
+res = np.linalg.lstsq(I, E)
+print(res)
