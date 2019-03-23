413185bd8de547cc5655f3e51d705676505cd683
Arkadiusz Kraus <arkadius.kraus@gmail.com>
2019-03-23 10:37:48 +0100

    [2] graph reading

--- 2/main.py
+++ 2/main.py
@@ -6,15 +6,17 @@ import networkx as nx
 
 def import_from_file(file):
     records = []
-
+    G = nx.Graph()
     with open(file, "r") as csvfile:
         reader = csv.reader(csvfile)
         for row in reader:
             records.append(row)
-    #map(records
+    G.add_nodes_from(list(map(lambda x: int(x[0]), records)))
+    for record in records:
+        G.add_edge(int(record[0]), int(record[1]), R=abs(int(record[2])))
+
+    return G
 
 
-G = nx.Graph()
-import_from_file("soc-sign-bitcoinalpha.csv")
-
-
+G = import_from_file("soc-sign-bitcoinalpha.csv")
+print(G.edges)
