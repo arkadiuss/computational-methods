da78720a1706aa938d3ac117c4aeba74b9332b9e
Arkadiusz Kraus <arkadius.kraus@gmail.com>
2019-03-23 10:57:50 +0100

    [2] kir 1

--- 2/main.py
+++ 2/main.py
@@ -12,11 +12,35 @@ def import_from_file(file):
         for row in reader:
             records.append(row)
     G.add_nodes_from(list(map(lambda x: int(x[0]), records)))
-    for record in records:
-        G.add_edge(int(record[0]), int(record[1]), R=abs(int(record[2])))
+    for i, record in enumerate(records):
+        print(record, i)
+        G.add_edge(int(record[0]), int(record[1]),  R=abs(int(record[2])), I=0, no=i)
 
     return G
 
 
-G = import_from_file("soc-sign-bitcoinalpha.csv")
-print(G.edges)
+def KirI(G):
+    eqs = []
+    edges_count = len(G.edges)
+    for n in G.nodes:
+        Is = [0 for i in range(edges_count)]
+        for e in G.edges(n):
+            print(e)
+            data = G.get_edge_data(*e)
+            Is[data['no']] = 1
+        print('\n\n')
+        eqs.append(Is)
+    return eqs
+
+
+#def KirII(G):
+
+
+def create_equations(G):
+    eqs = KirI(G)
+    print(eqs)
+
+
+G = import_from_file("small.csv")
+create_equations(G)
+#print(G.edges)
