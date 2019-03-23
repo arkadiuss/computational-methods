4a0e2a95878ab1fe6e56230687b65486bbcb01c3
Arkadiusz Kraus <arkadius.kraus@gmail.com>
2019-03-23 11:22:15 +0100

    [2] creating equations

--- 2/main.py
+++ 2/main.py
@@ -15,6 +15,7 @@ def import_from_file(file):
     for i, record in enumerate(records):
         print(record, i)
         G.add_edge(int(record[0]), int(record[1]),  R=abs(int(record[2])), I=0, no=i)
+        # G.add_edge(int(record[1]), int(record[0]),  R=abs(int(record[2])), I=0, no=i)
 
     return G
 
@@ -25,19 +26,31 @@ def KirI(G):
     for n in G.nodes:
         Is = [0 for i in range(edges_count)]
         for e in G.edges(n):
-            print(e)
             data = G.get_edge_data(*e)
-            Is[data['no']] = 1
+            print(e, data['no'])
+            Is[data['no']] = 1 if e[0] > e[1] else -1
         print('\n\n')
         eqs.append(Is)
     return eqs
 
 
-#def KirII(G):
+def KirII(G):
+    eq = []
+    edges_count = len(G.edges)
+    for cycle in nx.cycle_basis(G):
+        Is = [0 for i in range(edges_count)]
+        for i in range(len(cycle)):
+            n1 = cycle[i]
+            n2 = cycle[(i+1) % len(cycle)]
+            data = G.get_edge_data(n1,n2)
+            Is[data['no']] = data['R'] if n1 > n2 else -data['R']
+        eq.append(Is)
+    return eq
 
 
 def create_equations(G):
     eqs = KirI(G)
+    eqs += KirII(G)
     print(eqs)
 
 
