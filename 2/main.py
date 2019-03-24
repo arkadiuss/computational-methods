4a084e39769b0f7e5cdb7041cd4a64d2301f7c77
Przemek Jablecki <prz.jab98@gmail.com>
2019-03-24 16:46:56 +0100

    NK KURWA DODA TE JEBANE ETYKIETY Xd

--- 2/main.py
+++ 2/main.py
@@ -1,9 +1,9 @@
 import numpy as np
 import os
 import csv
-import mpl
 import networkx as nx
 import matplotlib.pyplot as plt
+from sympy.physics.quantum.tests.test_circuitplot import mpl
 
 
 def import_from_file(file):
@@ -29,9 +29,9 @@ def KirI(G):
         b.append(0)
         for e in G.edges(n):
             data = G.get_edge_data(*e)
-            #print(e, data['no'])
+            # print(e, data['no'])
             Is[data['no']] = 1 if e[0] > e[1] else -1
-        #print('\n\n')
+        # print('\n\n')
         eqs.append(Is)
     return eqs, b
 
@@ -67,7 +67,7 @@ G.add_edge(1, 4, R=0, sem=3, no=0)
 I, E = create_equations(G)
 res = np.linalg.lstsq(I, E, rcond=None)[0]
 
-#print(res)
+# print(res)
 
 
 for i, e in enumerate(G.edges):
@@ -80,16 +80,13 @@ digraph.add_nodes_from(G)
 max_I = -1
 
 for i, e in enumerate(G.edges):
-    #print(e, G[e[0]][e[1]]['I'])
+    # print(e, G[e[0]][e[1]]['I'])
     if G[e[0]][e[1]]['I'] > 0:
-        digraph.add_edge(max(e[0],e[1]), min(e[0],e[1]), I=G[e[0]][e[1]]['I'])
+        digraph.add_edge(max(e[0], e[1]), min(e[0], e[1]), I=G[e[0]][e[1]]['I'])
     else:
-        digraph.add_edge(min(e[0],e[1]), max(e[0],e[1]), I=abs(G[e[0]][e[1]]['I']))
+        digraph.add_edge(min(e[0], e[1]), max(e[0], e[1]), I=abs(G[e[0]][e[1]]['I']))
     if abs(G[e[0]][e[1]]['I']) > max_I:
         max_I = abs(G[e[0]][e[1]]['I'])
-
-
-
 
 pos = nx.layout.spring_layout(digraph)
 
@@ -101,7 +98,7 @@ edge_alphas = [(5 + i) / (M + 4) for i in range(M)]
 nodes = nx.draw_networkx_nodes(digraph, pos, node_size=node_sizes, node_color='blue')
 edges = nx.draw_networkx_edges(digraph, pos, node_size=node_sizes, arrowstyle='->',
                                arrowsize=10, edge_color=edge_colors,
-                               edge_cmap=plt.cm.Blues, width=2)
+                               edge_cmap=plt.cm.Blues, width=2, with_labels=True)
 # set alpha value for each edge
 for i in range(M):
     edges[i].set_alpha(edge_alphas[i])
@@ -113,5 +110,3 @@ plt.colorbar(pc)
 ax = plt.gca()
 ax.set_axis_off()
 plt.show()
-
-
