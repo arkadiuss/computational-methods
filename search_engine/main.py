42d5862684fd5aa2d806541680b8423f3fb76cdd
Arkadiusz Kraus <arkadius.kraus@gmail.com>
2019-05-20 01:54:19 +0200

    creating vector of words

--- search_engine/main.py
+++ search_engine/main.py
@@ -16,7 +16,7 @@ def read_csv_file(file):
 def read_articles():
     articles = []
     for a in ['articles/articles1.csv']:
-        articles.append(read_csv_file(a))
+        articles += read_csv_file(a)
     return articles
 
 
@@ -24,9 +24,40 @@ def remove_characters(article):
     return ''.join(c for c in article if c.isalpha() or c == ' ')
 
 
-def bow(article):
-    return [[x, article.count(x)] for x in article.split()]
+def prepare_article(article):
+    return remove_characters(article).lower().split(' ')
 
 
-article1 = remove_characters(read_articles()[0][-1][-1])
-print(bow(article1))
+def bag_of_words(article):
+    words = {}
+    for i in article:
+        words[i] = words.get(i, 0) + 1
+    return words
+
+
+def create_words_vector(articles):
+    res = set()
+    for k, i in enumerate(articles):
+        if k % 10000 == 0:
+            print("Processed " + str(k) + " articles")
+        bow = bag_of_words(i)
+        res = res.union(bow.keys())
+    return res
+
+
+print("Reading articles...")
+articles = read_articles()[:10000]
+
+print("Preparing...")
+#TODO:  keep also id
+with open('p_articles.csv', 'w') as f:
+    for i in range(len(articles)):
+        id = articles[i][0]
+        articles[i] = prepare_article(articles[i][-1])
+        f.write("{0}, {1}\n".format(id, articles[i]))
+
+print("Creating word vector...")
+words_vector = create_words_vector(articles)
+print(len(words_vector))
+with open('words.txt', 'w') as f:
+    f.write(repr(words_vector))
